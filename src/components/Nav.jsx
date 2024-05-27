dde582728bf3f7755796f87bde8c8d6bf493955b
Nour123amer <nouramer295@gmail.com>
2024-05-27 20:01:56 +0300

    react project

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+export default function Nav(){
+    return(
+        <>
+        <div className="container bg-sky-500/10 flex">
+            <h1>START FRAMEWORK</h1>
+
+            <ul>
+                <li className="white font-bold"><a href="">ABOUT</a></li>
+                <li className="white font-bold"><a href="">PORTFOLIO</a></li>
+                <li className="white font-bold"><a href="">CONTACT</a></li>
+            </ul>
+        </div>
+        </>
+    )
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../../components/Nav";
+import Background from "../../components/Background";
+import Footer from "../../components/Footer";
+import Copyright from "../../components/Copuright";
+export default function Home(){
+    return(
+        <>
+        <Nav />
+        <Background />
+        <Footer />
+        <Copyright />
+        </>
+    )
+}
